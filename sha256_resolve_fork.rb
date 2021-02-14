a9eaf7aebda302b2ebc79ac1acfb1a8b893cb092
Yamato Ashikaga <a.yamato.so.ri.00@gmail.com>
2021-02-14 17:18:45 +0900

    余計なコード削除

--- sha256_resolve_fork.rb
+++ sha256_resolve_fork.rb
@@ -7,8 +7,8 @@ window_size = 100000
 i = 0
 
 while true
-  run_worker = worker - 1
-  (1...worker).each { |_|
+  run_worker = worker
+  (1..worker).each { |_|
     _start = i
     _end = i + window_size
     i = i + window_size + 1
@@ -27,13 +27,9 @@ while true
 
   while true
     break if run_worker.zero?
-    begin
-      result = Process.waitpid2
-      run_worker = run_worker - 1
-      Process.exit 0 if result[1].success?
-      sleep 0.01
-    rescue Errno::ECHILD
-      # ignore (無限ループする危険あるけど一旦これで)
-    end
+    result = Process.waitpid2
+    run_worker = run_worker - 1
+    Process.exit 0 if result[1].success?
+    sleep 0.01
   end
 end
